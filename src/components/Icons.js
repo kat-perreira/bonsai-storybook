c5a7cf74a6be3b24d2843f057b82ae49902bb0df
kat-perreira <kat.perreira@gmail.com>
2019-03-20 13:45:36 -0700

    refactored for merge with master, took out line about debug lib in the preview-head

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -4,26 +4,7 @@ import PropTypes from "prop-types";
 export class HsIcon extends React.Component {
 
   render(props) {
-   
-    
-
-
-    // if (props.forElement !== true) {
-    //   const classes = props.class ? `hs-icon ${props.class}` : "hs-icon";
-    //   // include wrapper <div class="hs-icon"> for jsx component <HsIcon>
-    //   return (
-    //     <div class={classes} icon={props.icon}
-    //       {...(props.size && { size: props.size })}
-    //       {...(props.circle && { circle: true })}
-    //     >
-    //       {template}
-    //     </div>
-    //   );
-    // }
-    // console.log("skldjfklsdjfkl");
-    // exclude wrapper <div class="hs-icon"> for element <hs-icon>
-    //return template;
-    // return (<h1>{this.props.icon}</h1>);
+  
     return (
       <svg class={"icon-" + this.props.icon}>
         <use xlinkHref={"#icon-" + this.props.icon}/>
@@ -31,9 +12,6 @@ export class HsIcon extends React.Component {
     );
   }
 }
-
-
-
 HsIcon.propTypes = {
   icon: PropTypes.string.isRequired,     // required name of svg icon
   circle: PropTypes.bool,                  // circle around icon
--- src/components/Icons.stories.js
+++ src/components/Icons.stories.js
@@ -2,24 +2,8 @@ import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react/dist/client/preview";
 import React from "react";
 import { HsIcon } from "./Icons";
-// import PropTypes from "prop-types";
-//  //import { HsIcon } from "./Icons";
 
 
-
-// export default props => (
-//   <svg width="22" height="22" viewBox="0 0 1024 1024">
-//     <path d={icons[props.icon]} />
-//   </svg>
-
-
-
-
-storiesOf("Icons", module)
-  // .add("Trash", () => (
-  //    <svg class="icon-trashcan"><use xlinkHref="#icon-trashcan"></use></svg>
-
-  //  ));
 storiesOf("Icons", module)
   .add("Trash", () => (
     <div>
