4577b0acf5b6f8daf15306cca31964ca3822180a
kat-perreira <kat.perreira@gmail.com>
2019-04-05 09:57:18 -0700

    added css from product to icons

--- src/components/Icons.stories.js
+++ src/components/Icons.stories.js
@@ -4,9 +4,100 @@ import React from "react";
 import { HsIcon } from "./Icons";
 
 
-storiesOf("Icons", module)
-  .add("Trash", () => (
-    <div>
-      <HsIcon icon="trashcan" />
+storiesOf("Icons", module).add("HS Icons", () => (
+  <div class="icons">
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="trashcan" />
     </div>
-  ))
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="yield" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="directory" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="bookmark" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="notification" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="home" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="folder" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="person" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="globe" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="newspaper" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="thumb-up" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="recap" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="send" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="info" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="question" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="send" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="info" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="question" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="list" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="chart-pie" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="dna" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="coverage" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="menu-open" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="remove" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="screen-share" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="template-email" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="filter-full" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="copy" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="filter-full" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="spotoverviewdoc" />
+    </div>
+    <div class="hs-icon small">
+      <HsIcon class="hs-icon small" icon="pitch-experience" />
+    </div>
+  </div>
+));
